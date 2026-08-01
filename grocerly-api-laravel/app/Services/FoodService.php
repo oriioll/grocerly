0be1c1af824e7f007d35e273c8aa0d6ca0d6b6e2
oriioll <oriolplazas2@gmail.com>
2026-08-02 00:57:01 +0200

    refactor: use foodDTO to return food related jsons

--- grocerly-api-laravel/app/Services/FoodService.php
+++ grocerly-api-laravel/app/Services/FoodService.php
@@ -10,43 +10,50 @@ class FoodService
 {
     /**
      * Get all the food items from db
-     * @return Collection<int, Food> Collection of Food Model Objects
+     * @return array array of Food Model Objects
      * @author  Oriol Plazas
      * @since 30/07/2026
      * @see App\Models\Food.php
      */
-    public function getAll(): Collection
+    public function getAll(): array
     {
-        return Food::all();
+        $foods = Food::all();
+        $foodsDTO = [];
+        foreach ($foods as $food) {
+            $foodsDTO[] = new FoodDTO($food->name, $food->kcal, $food->category);
+        }
+        return $foodsDTO;
     }
 
     /**
      * Get a food by its id
      * @param int $foodId The id of the food
-     * @return Food The food with the id
+     * @return FoodDTO The food with the id
      * @author  Oriol Plazas
      * @since 30/07/2026
      * @see App\Models\Food.php
      */
-    public function getById(int $foodId): Food
+    public function getById(int $foodId): FoodDTO
     {
-        return Food::findOrFail($foodId);
+        $food = Food::findOrFail($foodId);
+        return new FoodDTO($food->name, $food->kcal, $food->category);
     }
 
     /**
      * Create food in db
      * @param FoodDTO $foodDTO food to insert
-     * @return Food Food inserted
+     * @return FoodDTO Food inserted
      * @author  Oriol Plazas
      * @since 01/08/2026
      * @see App\Models\Food.php
      */
-    public function create(FoodDTO $foodDTO): Food
+    public function create(FoodDTO $foodDTO): FoodDTO
     {
-        return Food::create([
+        $food =  Food::create([
             'name' => $foodDTO->name,
             'kcal' => $foodDTO->kcal,
             'category' => $foodDTO->category
         ]);
+        return new FoodDTO($food->name, $food->kcal, $food->category);
     }
 }
