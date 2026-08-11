224e04eb75735499808ce1add784f63e7f801187
oriioll <oriolplazas2@gmail.com>
2026-08-11 23:55:44 +0200

    feat: add Dashboard view

---
+++ grocerly-vue/src/views/Dashboard.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import { UserService } from '@/services/UserService';
+
+const router = useRouter();
+const user = ref('Guest');
+const userService = new UserService();
+
+onMounted(async () => {
+    try {
+        const response = await userService.getMe();
+        user.value = response?.name || 'Guest';
+    } catch (error: any) {
+        router.push('/register');
+    }
+})
+</script>
+
+<template>
+    <main>
+        <h1>Hello, {{ user }} </h1>
+    </main>
+</template>
+
+<style scoped>
+main {
+    width: 100%;
+    height: 50dvh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 50px;
+    gap: 1rem;
+
+}
+</style>
