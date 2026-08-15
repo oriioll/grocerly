2deb591d45f82e6b427d7c4f5155ffc5a4861b2a
oriioll <oriolplazas2@gmail.com>
2026-08-15 23:31:28 +0200

    feat: add ShoppingListCard component

---
+++ grocerly-vue/src/components/Dashboard/ShoppingListCard.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { ShoppingList } from '@/types/shoppingList';
+
+const props = defineProps<{
+    list: ShoppingList
+}>()
+</script>
+
+<template>
+    <div class="card">
+        <p>{{ props.list.foods.length }} items</p>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    width: 100%;
+    padding: 5px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: .25rem;
+    border: solid 1px var(--border-color);
+    cursor: pointer;
+    transition: all .2s ease;
+}
+
+.card:hover {
+    transform: translateY(-1px);
+}
+</style>
