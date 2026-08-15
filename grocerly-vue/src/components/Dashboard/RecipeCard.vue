be4aaefce3c5bece90f7c2333d5bc554931b4805
oriioll <oriolplazas2@gmail.com>
2026-08-15 23:03:31 +0200

    feat: add new RecipeCard component

---
+++ grocerly-vue/src/components/Dashboard/RecipeCard.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import type { Recipe } from '@/types/recipe';
+
+const props = defineProps<{
+    recipe: Recipe
+}>()
+</script>
+
+<template>
+    <div class="card">
+        <div class="top">
+            <h6>{{ props.recipe.name }}</h6>
+            <p>{{ props.recipe.servings }}servings</p>
+        </div>
+        <p>{{ props.recipe.foods.length }} ingredients</p>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    width: 100%;
+    padding: 5px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    gap: .25rem;
+    border: solid 1px var(--border-color);
+    cursor: pointer;
+    transition: all .2s ease;
+}
+
+.card:hover {
+    transform: translateY(-1px);
+}
+
+.top {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: .25rem;
+}
+</style>
